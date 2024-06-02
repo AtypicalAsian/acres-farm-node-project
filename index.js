6081b2a283e5dd261536ebfc3c52de5ee2f68623
Nicolas Nguyen <nicnguyen2702@gmail.com>
2024-06-02 15:57:00 -0400

    added slugify

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 //======== SERVER ==============
@@ -21,6 +23,9 @@ const tempCard = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const prodData = JSON.parse(data);
+
+const slug = prodData.map((el) => slugify(el.productName, { lower: true }));
+console.log(slug);
 
 const ser = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
